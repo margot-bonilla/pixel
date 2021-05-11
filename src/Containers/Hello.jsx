bdbb6b5f8c313b90a3e33012d27aab49f71d5e8f
Margot Bonilla <margot.bonilla.castany@gmail.com>
2021-05-11 18:52:43 +0100

    test

---
+++ src/Containers/Hello.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello folks!
+            </div>
+        );
+    }
+}
